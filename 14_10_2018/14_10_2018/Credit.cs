7b3cea983ee0792188783ad9393be170362637ae
Yasin <Yasin@DESKTOP-JR5VDO0>
2018-10-14 18:16:31 +0400

    thecodding

---
+++ 14_10_2018/14_10_2018/Credit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_10_2018
+{
+    class Credit
+    {
+        public int Months { get; set; }
+        public double Price { get; set; }
+        public double MonthlyPercent { get; set; }
+        public string Currency { get; set; }
+
+        private DateTime DateTime;
+
+        public Credit(int ay,double qiymet,double ayliqfaiz)
+        {
+            this.Months = ay;
+            this.Price = qiymet;
+            this.MonthlyPercent = ayliqfaiz;
+            this.Currency = "AZN";
+        }
+
+        public void Calc()
+        {
+            double total= this.Price+(this.Price * ((this.Months * this.MonthlyPercent) / 100));
+            double monthlyCash = total / this.Months;
+            double coins = monthlyCash % 1;
+            monthlyCash -= coins;
+            coins *= this.Months;
+            this.DateTime = DateTime.Now;
+
+            for (int i = 0; i < this.Months; i++)
+            {
+                if ((i + 1) == this.Months)
+                {
+                    monthlyCash += coins;
+                }
+                Console.WriteLine("Odenis Tarixi: "+this.DateTime.ToString("dd.MM.yyy")+" "+"Mebleg: "+monthlyCash+" "+this.Currency);
+                this.DateTime = this.DateTime.AddMonths(1);
+            }
+        }
+    }
+}
---
+++ 14_10_2018/14_10_2018/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_10_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----------------------------Sisteme Xos Gelmissiniz----------------------------");
+
+            string selectText = "";
+            int select = 0;
+
+            Console.WriteLine("Seciminiz edin:");
+            Console.WriteLine("1.Esya Al");
+            Console.WriteLine("2.Sistemden Cixis");
+
+            do
+            {
+
+                selectText = Console.ReadLine();
+                while (!int.TryParse(selectText,out select))
+                {
+                    Console.WriteLine("Duzgun Sekilde seciminizi edin");
+                    selectText = Console.ReadLine();
+                }
+
+                switch (select)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("-------------------------Credit Card System----------------------");
+
+                        //---------------------------------------------------------------
+                        Console.WriteLine("Nece ayliq kredit gotumek isdeyirsiniz?");
+                        string monthStr = Console.ReadLine();
+                        int month = 0;
+                        while (!int.TryParse(monthStr,out month) || month<=0)
+                        {
+                            Console.WriteLine("Duzgun Sekilde ay sayini daxil edin");
+                            monthStr = Console.ReadLine();
+                        }
+
+                        //---------------------------------------------------------------
+                        Console.WriteLine("Goturmek isdediyiniz esyanin qiymetini daxil edin");
+                        string priceStr = Console.ReadLine();
+                        double price = 0;
+                        while (!double.TryParse(priceStr, out price) || price<=0)
+                        {
+                            Console.WriteLine("Esyanin qiymetini duzgun sekilde daxil edin");
+                            priceStr = Console.ReadLine();
+                        }
+
+                        //---------------------------------------------------------------
+                        Console.WriteLine("Ayliq nece faiz kredit olmasini isdeyirsiniz?");
+                        string monthPercentStr = Console.ReadLine();
+                        double monthPercent = 0;
+                        while (!double.TryParse(monthPercentStr, out monthPercent) || monthPercent<0)
+                        {
+                            Console.WriteLine("Ayliq kredit faizini duzgun sekilde daxil edin");
+                            monthPercentStr = Console.ReadLine();
+                        }
+                        Console.WriteLine("--------------------------------");
+
+                        //---------------------------------------------------------------
+                        Console.Clear();
+                        Console.WriteLine("------------------Neticeniz---------------");
+                        Credit crd = new Credit(month, price, monthPercent);
+                        crd.Calc();
+
+                        //---------------------------------------------------------------
+
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Sistemden istifade etdiyiniz ucun tesekkurler");
+                        break;
+                    default:
+                        Console.WriteLine("--------------------------");
+                        Console.WriteLine("Size teklif edilen secimlerden birini secin");
+                        break;
+                }
+
+            } while (select!=0);
+        }
+    }
+}
